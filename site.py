26efb912777689215a3c68c2d5480bafe54edadc
Vikum Semage <cheesebob7104@gmail.com>
2021-01-16 10:12:56 -0500

    added explanation toggle

--- site.py
+++ site.py
@@ -3,10 +3,9 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-st.sidebar()
 
-st.title("Hurrican Damage Detection")
-st.write("Hi! This model takes satellite images from earth and depicts whether they have been damaged by a satellite. To begin, upload a png or jpeg file below.")
+st.title("Hurricane Damage Detection")
+st.write("Hi! This model takes satellite images from earth and depicts whether they have been damaged by a Hurricane. To begin, upload a png or jpeg file below.")
 
 def load_image(image_file):
     img = Image.open(image_file)
@@ -16,8 +15,18 @@ def load_image(image_file):
 image_file = st.file_uploader(label="Upload your satellite image", type=["png", "jpeg", "jpg"])
 
 if image_file != None:
-    st.image(load_image(image_file))
+        st.image(load_image(image_file))
 
+col_1, col_2 = st.beta_columns([3,1])
 
+explanation = False
 
+with col_1:
+    if st.button("view explanation"):
+        explanation = True
+        with col_2:
+            if st.button("hide explanation"):
+                explanation = False
 
+if explanation:
+    st.write("here is an explanation")
