117812fb23a0178fda623e27ab6e22d9771eca78
Ryan Awad <ryanawad@yahoo.com>
2021-01-16 18:16:59 -0500

    Worked on pages

--- site.py
+++ site.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="centered"
 )
 
-activity = st.sidebar.selectbox("Analyze", ["Model", "Map", "Model Fitting Data"])
+activity = st.sidebar.selectbox("Analyze", ["Model", "About", "Visualize the Data"])
 
 if activity == "Model":
     def process_img(img):
@@ -61,7 +61,7 @@ if activity == "Model":
 
     st.title("Hurricane Damage Detection")
     st.write("""
-    This model takes satellite images from earth and depicts whether they have been damaged by a Hurricane. To begin, upload an image file below.   
+    This model uses 3 channel (RGB) satellite images from earth and depicts whether they have been damaged by a Hurricane. To begin, upload an image file below.   
     """)
 
     image_file = st.file_uploader(label="Upload your satellite image", type=["png", "jpeg", "jpg"])
@@ -75,18 +75,28 @@ if activity == "Model":
             elif pred == class_names[1]:
                 st.success(f'{pred} was detected in the uploaded image.')
 
-elif activity == "Map":
-    map_data = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-        columns=['lat', 'lon']
-    )
-    st.map(map_data)
 
-elif activity == "Model Fitting Data":
-    st.write("in progress!")
+elif activity == "About":
+    st.title("About")
+    st.write("""
+    ### How the Model Works
+    Hurricanes cause lots of devastation and affect many people physically, mentally and economically. For our model, we utilized a dataset that contains satellite images of both damaged and undamaged areas in Texas after Hurricane Harvey unfortunately struck in 2017. For this reason we decided that we would create an application that would detect whether an area has been damaged by a hurricane. This would be useful for detecting if an area was damaged by a hurricane much earlier. This is great because it allows help to be sent earlier to the designated damaged area. Instead of having to wait for a damaged area to be reported, which takes longer, you could get a live satellite image of each area and see exactly which areas need help. 
+    
+    ### More in Depth
+    To build our model, we used *TensorFlow* to create a *convolutional neural network (CNN)* to detect patterns and edges in the satellite images. The optimizer used to compile model was the *Adam optimizer* with a learning rate of `1e-4`. For the loss function, we used *binary cross-entropy*. For more info on the architecture of our model, check out our GitHub repository. 
 
+    ### Where We Got The Data
+    The data is composed of satellite images from Texas after Hurricane Harvey struck in 2017. The data is divided into 2 groups, `damage` and `no_damage`. This data allows us to train a model to detect if an area in **any** satellite image was damaged by a hurricane. The data used to train the model was taken from [here](https://ieee-dataport.org/open-access/detecting-damaged-buildings-post-hurricane-satellite-imagery-based-customized).
+    """)
+
+elif activity == "Visualize the Data":
+    st.title("Visualize the Data")
+    fit_data = pd.read_csv("visual_data/fit_history.csv")
+    st.write('### Model Train and Validation Loss')
+    st.line_chart(fit_data[["Train Loss", "Validation Loss"]])
+    st.write('### Model Train and Validation Accuracy')
+    st.line_chart(fit_data[["Train Accuracy", "Validation Accuracy"]])
 else:
-    st.title("Error 404: Page Not Found")
-    st.write("I think you're a little lost...")
+    st.title("Page Not Found")
 
 
